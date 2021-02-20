954205b2473a8e06cf960e077fcbe1bcd9f54bad
David Whittington <djwhitt@gmail.com>
2021-02-20 10:13:45 -0600

    Initialize Arweave library in events namespace
    I suspect this will move into another namespace, but for the MVP I
    expect most Arweave library interaction to happen via events. I might as
    well just put it there for now.

--- src/cljs/org/weavemarks/events.cljs
+++ src/cljs/org/weavemarks/events.cljs
@@ -3,7 +3,9 @@
    [re-frame.core :as re-frame]
    [org.weavemarks.db :as db]
    [day8.re-frame.tracing :refer-macros [fn-traced]]
-   ))
+   ["arweave" :default Arweave]))
+
+(def arweave (.init Arweave))
 
 (re-frame/reg-event-db
  ::initialize-db
