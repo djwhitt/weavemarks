a47591dbf0c8e57aff9a9230c611d187faa753a5
David Whittington <djwhitt@gmail.com>
2021-02-20 10:40:06 -0600

    Add minimal UI for using a wallet and adding a bookmark
    This is very much not recommended wallet UI or HTML structure. I'm going
    for getting something minimal working and then refining it.

--- src/cljs/org/weavemarks/views.cljs
+++ src/cljs/org/weavemarks/views.cljs
@@ -1,11 +1,22 @@
 (ns org.weavemarks.views
   (:require
    [re-frame.core :as re-frame]
-   [org.weavemarks.subs :as subs]
-   ))
+   [org.weavemarks.subs :as subs]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
-     [:h1 "Hello from " @name]
+     [:h1 "Weavemarks"]
+     [:h2 "Wallet"]
+     [:p
+      [:textarea]]
+     [:p
+      [:button "Use Wallet"]]
+     [:h2 "New Bookmark"]
+     [:h3 "URL"]
+     [:input]
+     [:h3 "Description"]
+     [:input]
+     [:h2 "Bookmarks"]
+     ;; TODO add bookmark list
      ]))
